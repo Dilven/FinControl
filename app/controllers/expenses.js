4bbc597cce3de6de70e686a40eab359391092158
Dilven <mateusiakkrystian@gmail.com>
2017-10-27 18:01:37 +0200

    views(analysis,expenses)

---
+++ app/controllers/expenses.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    router = express.Router();
+
+module.exports = function (app) {
+    app.use('/expenses', router);
+};
+
+router.get('/', function (req, res, next) {
+    
+    res.render('expenses', {
+        title: 'Panel glowny',
+        lastExpenses: [
+            { name: 'Piwo', amount: '2.99', date: new Date('2017', '10', '30'), category: 'Alkohol' },
+            { name: 'Zakupy na obiad', amount: '30.99', date: new Date('2017', '10', '27'), category: 'Gospodarstwo domowe' },
+            { name: 'Kino', amount: '20.99', date: new Date('2017', '10', '25'), category: 'Rozrywka'},
+            { name: 'Mecz Arsenal vs Chelsea', amount: '100', date: new Date('2017', '10', '28'), category: 'Rozrywka'}
+        ],
+        user: {
+            name: 'Jan',
+            surname: 'Kowalski',
+            budget: '500.29'
+        }
+    });
+    
+});
