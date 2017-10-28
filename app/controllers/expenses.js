ebcf55d99556e6988b018f216ffbd31a3087518b
Dilven <mateusiakkrystian@gmail.com>
2017-10-28 13:24:10 +0200

    views

--- app/controllers/expenses.js
+++ app/controllers/expenses.js
@@ -9,17 +9,26 @@ router.get('/', function (req, res, next) {
     
     res.render('expenses', {
         title: 'Panel glowny',
-        lastExpenses: [
+        expenses: [
             { name: 'Piwo', amount: '2.99', date: new Date('2017', '10', '30'), category: 'Alkohol' },
             { name: 'Zakupy na obiad', amount: '30.99', date: new Date('2017', '10', '27'), category: 'Gospodarstwo domowe' },
             { name: 'Kino', amount: '20.99', date: new Date('2017', '10', '25'), category: 'Rozrywka'},
-            { name: 'Mecz Arsenal vs Chelsea', amount: '100', date: new Date('2017', '10', '28'), category: 'Rozrywka'}
+            { name: 'Mecz Arsenal vs Chelsea', amount: '100', date: new Date('2017', '10', '28'), category: 'Rozrywka'},
+            { name: 'Mecz Arsenadasl vs Chelsea', amount: '100', date: new Date('2017', '10', '28'), category: 'Rozrywka'},
+            { name: 'Mecz Arsenasdaal vs Chelsea', amount: '100', date: new Date('2017', '10', '28'), category: 'Rozrywka'},
+            { name: 'Piwo', amount: '2.99', date: new Date('2017', '10', '30'), category: 'Alkohol' },
+            { name: 'Zakupy na obiad', amount: '30.99', date: new Date('2017', '10', '27'), category: 'Gospodarstwo domowe' },
+            { name: 'Kino', amount: '20.99', date: new Date('2017', '10', '25'), category: 'Rozrywka'},
+            { name: 'Mecz Arsenal vs Chelsea', amount: '100', date: new Date('2017', '10', '28'), category: 'Rozrywka'},
+            { name: 'Mecz Arsenadasl vs Chelsea', amount: '100', date: new Date('2017', '10', '28'), category: 'Rozrywka'},
+            { name: 'Mecz Arsenasdaal vs Chelsea', amount: '100', date: new Date('2017', '10', '28'), category: 'Rozrywka'},
+            { name: 'Piwo', amount: '2.99', date: new Date('2017', '10', '30'), category: 'Alkohol' },
+            { name: 'Zakupy na obiad', amount: '30.99', date: new Date('2017', '10', '27'), category: 'Gospodarstwo domowe' },
+            { name: 'Kino', amount: '20.99', date: new Date('2017', '10', '25'), category: 'Rozrywka'},
+            { name: 'Mecz Arsenal vs Chelsea', amount: '100', date: new Date('2017', '10', '28'), category: 'Rozrywka'},
+            { name: 'Mecz Arsenadasl vs Chelsea', amount: '100', date: new Date('2017', '10', '28'), category: 'Rozrywka'},
+            { name: 'Mecz Arsenasdaal vs Chelsea', amount: '100', date: new Date('2017', '10', '28'), category: 'Rozrywka'}
         ],
-        user: {
-            name: 'Jan',
-            surname: 'Kowalski',
-            budget: '500.29'
-        },
         navigation: [
             { name: 'Dzis'},
             { name: 'W tygodniu'},
