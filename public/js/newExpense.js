134b24b4c15c6586138ab40128a7484fd9d953bb
Dilven <mateusiakkrystian@gmail.com>
2017-11-02 01:27:43 +0100

    adding validation to newExpense.js

--- public/js/newExpense.js
+++ public/js/newExpense.js
@@ -1,36 +1,41 @@
+var element = document.getElementById('addExpense');
 
-var dialog = document.querySelector('dialog');
-var showDialogButton = document.querySelector('#addExpense');
-if (! dialog.showModal) {
-  dialogPolyfill.registerDialog(dialog);
-}
-showDialogButton.addEventListener('click', function() {
-  dialog.showModal();
-});
-dialog.querySelector('.close').addEventListener('click', function() {
-  dialog.close();
-});
-
-
-(function() {
-  'use strict';
-  var snackbarContainer = document.querySelector('#notificationBar');
-  var acceptExpenseButton = document.querySelector('#acceptExpense');
-  var handler = function(event) {
-    acceptExpenseButton.style.backgroundColor = '';
-  };
-  acceptExpenseButton.addEventListener('click', function() {
-    'use strict';
-    //acceptExpenseButton.style.backgroundColor = '#' +
-      //  Math.floor(Math.random() * 0xFFFFFF).toString(16);
+if (element != null) {
+    var dialog = document.querySelector('dialog');
+    var showDialogButton = document.querySelector('#addExpense');
+    if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    showDialogButton.addEventListener('click', function() {
+      dialog.showModal();
+    });
+    dialog.querySelector('.close').addEventListener('click', function() {
+      dialog.close();
+    });
     
-    var data = {
-      message: 'Dodano nowy wydatek.',
-      timeout: 4000,
-      actionHandler: handler,
-      actionText: 'Cofnij'
-    };
-    snackbarContainer.MaterialSnackbar.showSnackbar(data);
-    dialog.close();
-  });
-}());
+    
+    (function() {
+      'use strict';
+      var snackbarContainer = document.querySelector('#notificationBar');
+      var acceptExpenseButton = document.querySelector('#acceptExpense');
+      var handler = function(event) {
+        acceptExpenseButton.style.backgroundColor = '';
+      };
+      acceptExpenseButton.addEventListener('click', function() {
+        'use strict';
+        //acceptExpenseButton.style.backgroundColor = '#' +
+          //  Math.floor(Math.random() * 0xFFFFFF).toString(16);
+        
+        var data = {
+          message: 'Dodano nowy wydatek.',
+          timeout: 4000,
+          actionHandler: handler,
+          actionText: 'Cofnij'
+        };
+        snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        dialog.close();
+      });
+    }());
+    
+}
+
