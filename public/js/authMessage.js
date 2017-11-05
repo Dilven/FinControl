88116bc027b736300e7f13feb9a5698bb2ba5728
Dilven <mateusiakkrystian@gmail.com>
2017-11-05 15:20:33 +0100

    delete authMessage script

--- public/js/authMessage.js
+++
@@ -1,9 +0,0 @@
-r(function(){
-    var snackbarContainer = document.querySelector('#demo-toast-example');
-    var data = { message: '<%= message %>'};
-
-    snackbarContainer.MaterialSnackbar.showSnackbar(data);
-});
-
-function r(f){ /in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
-    
