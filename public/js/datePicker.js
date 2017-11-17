17fc37f45a41d01dbe29a4a250b582f44feed050
Dilven <mateusiakkrystian@gmail.com>
2017-11-17 23:55:14 +0100

    add DataPicker but z-index to fix

--- public/js/datePicker.js
+++ public/js/datePicker.js
@@ -1,15 +1,11 @@
-
-
 $( document ).ready(function() {
-    console.log( "Milena!" );
+    
     var dialog = new mdDateTimePicker.default({
         type: 'date'
     });
-    
+    console.log(dialog);
     var toggleButton = document.getElementById('dataPicker');
     toggleButton.addEventListener('click', function() {
-        dialog.className += " dataPicker-zindex";
-        console.log("Milena3")
         dialog.toggle();
     });
     
